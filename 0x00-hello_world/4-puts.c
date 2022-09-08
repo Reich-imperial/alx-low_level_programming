13972435e28f0db98c1d9db6b210b928a9102797
Reich-imperial <olanipekunayodele01@gmail.com>
2022-09-08 14:40:02 -0700

    Latest

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this prints: Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
