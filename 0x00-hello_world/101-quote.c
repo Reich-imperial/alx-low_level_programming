18727e0338fd68453a128fcae29748b6efcf185a
Reich-imperial <olanipekunayodele01@gmail.com>
2022-09-08 17:05:28 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  * description: you are not allowed to use any function listed
@@ -6,7 +7,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 61);
-
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora korpar, 2015-10-19"));
+	return (1);
 }
+
