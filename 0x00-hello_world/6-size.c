b7701fae7b963cccba4bf3874c660891c023299d
Reich-imperial <olanipekunayodele01@gmail.com>
2022-09-08 17:39:25 -0700

    what

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 	long long int c;
 	char d;
 	float f; 
-
 	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
