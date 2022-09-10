1955836e97b24289fac3bb0af7192f938c762d02
Reich-imperial <olanipekunayodele01@gmail.com>
2022-09-10 14:08:40 -0700

    james

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive
+ *
+ * Description: using the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
