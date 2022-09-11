d517086db8603b3c6b1ac469b200dd3afa3331dd
Reich-imperial <olanipekunayodele01@gmail.com>
2022-09-11 00:50:49 -0700

    it is sunday

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabets in lowercase, and then in uppercase
- *
- * Return: 0
+ * main - Prints the alphabet in lowercase and then in uppercase.
+ * Return: Always 0
  */
 
 int main(void)
 {
 	char letter;
 
-	for (letter = "a"; letter <= "z"; letter++)
-	{
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	}
-	for (letter = "A"; letter <= "Z"; letter++)
-	{
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
-	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
