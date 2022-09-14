c4c73fb64c8919fec2d7141540f74dc66c50d14f
Reich-imperial <olanipekunayodele01@gmail.com>
2022-09-13 21:06:41 -0700

    las

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * adds - adds two integers
+ * @num1: First arguement
+ * @num2: Second Argeuement
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
