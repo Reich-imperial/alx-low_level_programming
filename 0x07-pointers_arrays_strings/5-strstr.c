ce3622e2c51e2d12cc90847d97de3ac262c46693
Reich-imperial <olanipekunayodele01@gmail.com>
2022-09-26 15:56:57 -0700

    task 5 and 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
- * _strstr - a function that locates substring
- * @haystack: string to be tested
- * @needle: substring to be searched
- * Return: returns a pointer to the beginning
+ * _strstr - afunction that locates a substring
+ * @haystack: to be tasted
+ * @needle: the substring to be searched for
+ * Return: returns a pointer
  */
 
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j = 0, k;
 
-	for (i = 0; haystack[i] != 0; 1++)
+	for (i = 0; haystack[i] != 0; i++)
 	{
 		k = i;
 		j = 0;
