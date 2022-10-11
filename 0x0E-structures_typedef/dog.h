3273d4c456c8c358c810f446606dbc248fead3d5
Reich-imperial <olanipekunayodele01@gmail.com>
2022-10-10 17:43:21 -0700

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,13 @@
 #define DOG_H
 
 /**
- * * struct dog - a new type: struct dog with the following elements
- * * @name: the name of the dog
- * * @age: the age of the dog
- * * @owner: the owner of the dog
- * * Description: a struct that houses a string and a float
- * */
+ * struct dog - a new type: struct dog with the following elements
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ * Description: a struct that houses a string and a float
+ */
+
 struct dog
 {
 		char *name;
